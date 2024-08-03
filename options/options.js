9ae2772d3c9b7b10a5c16d618b742f55185513c0
Crowdbotics <team@crowdbotics.com>
2024-08-03 01:07:25 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "teaco-48938",
+  url: "https://teaco-48938.botics.co",
+  api: "https://teaco-48938.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
